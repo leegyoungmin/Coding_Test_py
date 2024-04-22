bba7a4b59683d634f3604643109c5fa36f347295
이경민 <cow970814@naver.com>
2024-04-22 22:18:10 +0900

    feat : 240422

---
+++ 백준/240422/괄호 회전하기.py
@@ -0,0 +1,35 @@
+
+def lotation(s, cnt):
+    if cnt == 0:
+        return s
+
+    new_santance = s[cnt:] + s[0:cnt]
+    return new_santance
+def solution(s):
+    prefixs = ["[", "{", "("]
+    answer = 0
+
+    for i in range(0, len(s)):
+        santance = lotation(s, i)
+        values = []
+
+        for char in santance:
+            if len(values) == 0:
+                values.append(char)
+            else:
+                last = values[-1]
+                if last in prefixs:
+                    if (last == "[" and char == "]") or (last == "{" and char == "}") or (last == "(" and char == ")"):
+                        del values[-1]
+                    else:
+                        values.append(char)
+                else:
+                    values.append(char)
+        if len(values) == 0:
+            answer += 1
+    return answer
+
+if __name__ == '__main__':
+    input = input()
+    a = solution(input)
+    print(a)
