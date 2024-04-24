6ac8cc4e50e6fe54310b0b9fbeb8d5bb24889805
leegyoungmin <cow970814@naver.com>
2024-04-24 17:58:15 +0900

    feat : 문제 풀이 추가

--- 프로그래머스/240424/주식가격.py
+++ 프로그래머스/240424/주식가격.py
@@ -1,5 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
+"""
+- 스택을 활용한 문제
+- O(n^2)의 문제 풀이는 시간을 초과하게 된다.
+- 스택이 비어있지 않고, 이전의 가격 보다 떨이지는 경우에 연산을 통해서 가격을 확정짓는다.
+- 스택 내에는 현재 시점에서 가격이 떨어지지 않은 가격만 존재하게 된다.
+"""
+
 def solution(prices):
     stack = [0]
     n = len(prices)
