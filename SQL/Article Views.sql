87905b465660a8a2a8cad558f98f8f3941bc6d0f
이경민 <cow970814@naver.com>
2024-04-27 13:58:35 +0900

    feat : 24.04.27

---
+++ SQL/Article Views.sql
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+
+select distinct author_id as id from Views
+where author_id = viewer_id
+order by author_id
---
+++ SQL/Replace Employees.sql
@@ -0,0 +1,2 @@
+# https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
+select eu.unique_id, e.name from Employees e left outer join EmployeeUNI eu on eu.id = e.id
