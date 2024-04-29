b8964a499143efbdb2fded23591a67c50e21ca7d
이경민 <cow970814@naver.com>
2024-04-29 20:18:32 +0900

    feat : 24.04.29

---
+++ 백준/240429/요세푸스문제.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/1158
+
+"""
+1 2 3 4 5 6 7
+
+1번째 제거 -> 3 [1,2,4,5,6,7]
+2번째 제거 -> 
+
+"""
+
+NK = input().split(" ")
+n = int(NK[0])
+k = int(NK[1])
+
+q = list(range(1, n + 1))
+
+result = []
+front = 1
+
+while len(result) < n:
+    temp = q.pop(0)
+
+    if front % k == 0 and front != 0:
+        result.append(temp)
+    else:
+        q.append(temp)
+    
+    front += 1
+
+print('<', end='')
+for idx in range(len(result)):
+    if idx != n - 1:
+        print(f'{result[idx]}, ', end="")
+    else:
+        print(f'{result[idx]}', end="")
+print(">", end="")
